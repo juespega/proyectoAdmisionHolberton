0f9427a8602dd82f728638a275791b4f0a77ac16
juespega <juespega666@gmail.com>
2023-01-24 00:00:00 -0500

    Ok disqus comments and be social

--- behavior.js
+++ behavior.js
@@ -1,11 +1,20 @@
 document.addEventListener("DOMContentLoaded", function (event) {
   var thumbnailElement = document.getElementById("smart_thumbnail");
+  var thumbnailElement2 = document.getElementById("smart_thumbnail_2");
 
   thumbnailElement.addEventListener("click", function () {
     if (thumbnailElement.className === "small") {
       thumbnailElement.className = "smart_thumbnail";
     } else {
       thumbnailElement.className = "small";
+    }
+  });
+
+  thumbnailElement2.addEventListener("click", function () {
+    if (thumbnailElement2.className === "small") {
+      thumbnailElement2.className = "smart_thumbnail_2";
+    } else {
+      thumbnailElement2.className = "small";
     }
   });
 });
